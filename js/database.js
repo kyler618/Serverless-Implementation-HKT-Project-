3bda7a04e882ea7aee9e37a4ed2b135e15eb9f17
ITLetsGo <cytangao@connect.ust.hk>
2019-07-15 12:19:07 +0800

    update

--- js/database.js
+++ js/database.js
@@ -122,7 +122,7 @@ if (!_config.api.invokeUrl) {
             option.value = users;
             $('#itemModel-maintain-select').append(option);
           });
-          console.log('user done 1');
+          console.log('user done 2');
         }
       });
     }
