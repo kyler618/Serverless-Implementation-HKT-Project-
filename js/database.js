600bed1f1a845a0ad90f10df54c9e933776ce176
ITLetsGo <cytangao@connect.ust.hk>
2019-07-18 17:23:30 +0800

    update

--- js/database.js
+++ js/database.js
@@ -44,7 +44,7 @@ if (!_config.api.invokeUrl) {
       alert('An error occured:\n' + jqXHR.responseText);
     }
   };
-  console.log('version 7');
+  console.log('version 8');
 
   // on start
 
@@ -871,40 +871,33 @@ if (!_config.api.invokeUrl) {
     event.preventDefault();
     let previousPk;
     const itemForm = document.getElementById('itemForm');
-    // console.log($('tr').not(':first'));
-    // const index = Array.from($("td." + attributes[0])).map(x=>x.classList[1]).indexOf(event.target.classList[1]);
     const index = Array.from($('tr').not(':first')).map(x=>x.id).indexOf(event.target.classList[1]);
-    console.log(index);
-    const item = storedItem[index-1];
-    console.log(item);
-    // for( let x=Object.keys(item).length-1 ; x>=0 ; x-- )
-    // {
-    //   const fieldName = Object.getOwnPropertyNames(item)[x];
-    //   const field = createFormInput('form_Input', fieldName, true);
-    //   const value = createFormInput('form_Input', Object.values(item)[x], true);
-    //   field.classList.add('input-group-text');
-    //   value.classList.add('form-control');
-    //   if(Object.getOwnPropertyNames(item)[x] == attributes[0])
-    //   {
-    //     field.id = attributes[0];
-    //     itemForm.insertBefore(field, itemForm.childNodes[0]);
-    //     itemForm.insertBefore(value, itemForm.childNodes[1]);
-    //     itemForm.insertBefore(document.createElement("P"), itemForm.childNodes[2]);
-    //   }
-    //   else
-    //   {
-    //     field.classList.add(fieldName);
-    //     value.classList.add(fieldName);
-    //     itemForm.appendChild(field);
-    //     itemForm.appendChild(value);
-    //     const button = createRemoveButton();
-    //     button.id = fieldName;
-    //     itemForm.appendChild(button);
-    //     itemForm.appendChild(document.createElement("P"));
-    //   }
-    // }
-    // document.getElementById('itemModel').style.display='block';
-    // buttonClick();
+    const item = storedItem[index];
+    for( let x=Object.keys(item).length-1 ; x>=0 ; x-- ){
+      const fieldName = Object.getOwnPropertyNames(item)[x];
+      const field = createFormInput('form_Input', fieldName, true);
+      const value = createFormInput('form_Input', Object.values(item)[x], true);
+      field.classList.add('input-group-text');
+      value.classList.add('form-control');
+      if(Object.getOwnPropertyNames(item)[x] == attributes[0]){
+        field.id = attributes[0];
+        itemForm.insertBefore(field, itemForm.childNodes[0]);
+        itemForm.insertBefore(value, itemForm.childNodes[1]);
+        itemForm.insertBefore(document.createElement("P"), itemForm.childNodes[2]);
+      }
+      else{
+        field.classList.add(fieldName);
+        value.classList.add(fieldName);
+        itemForm.appendChild(field);
+        itemForm.appendChild(value);
+        const button = createRemoveButton();
+        button.id = fieldName;
+        itemForm.appendChild(button);
+        itemForm.appendChild(document.createElement("P"));
+      }
+    }
+    document.getElementById('itemModel').style.display='block';
+    buttonClick();
 
   }
 
