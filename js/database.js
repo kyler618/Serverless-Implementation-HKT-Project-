ee5a890f62c5fcbd676a2f6c7710ef1cdbf79dcd
ITLetsGo <cytangao@connect.ust.hk>
2019-07-19 10:24:08 +0800

    update

--- js/database.js
+++ js/database.js
@@ -833,7 +833,7 @@ if (!_config.api.invokeUrl) {
             else
             {
               // check user whether delete any record
-              // data.delete = (deleteAttr.length!=0)? deleteAttr:null;
+              data.delete = (deleteAttr.length!=0)? deleteAttr:null;
             }
             console.log('update', data);
             request(data, handleUpdateResponse);
