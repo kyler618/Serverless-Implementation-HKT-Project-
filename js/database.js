12a457cab3a7b62b9c2ca5554840101e7d8b96a5
ITLetsGo <cytangao@connect.ust.hk>
2019-07-22 15:58:19 +0800

    update

--- js/database.js
+++ js/database.js
@@ -146,7 +146,6 @@ if (!_config.api.invokeUrl) {
             const option = document.createElement('option');
             option.appendChild( document.createTextNode(user) );
             option.value = user;
-            // console.log(user, field_Engineer);
             if(user==field_Engineer){
               option.selected = true;
             }
