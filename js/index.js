63d838a6ebfecb6824433f78ea363c7cf2ad3606
ITLetsGo <cytangao@connect.ust.hk>
2019-07-23 18:07:51 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 $(function onDocReady() {
   $('#signinForm').submit(signin);
   function signin(event){
@@ -27,3 +26,38 @@ $(function onDocReady() {
       });
     }
 });
+
+// Prepare for future use
+// function handleChangePassword(event){
+//   var oldPassword = $('#oldPasswordInputChangePassword').val();
+//   var newPassword = $('#newPasswordInputChangePassword').val();
+//   var newPassword2 = $('#newPassword2InputChangePassword').val();
+//   event.preventDefault();
+//   if (newPassword === newPassword2) {
+//     var cognitoUser = userPool.getCurrentUser();
+//     if(cognitoUser != null){
+//       var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+//           Username: cognitoUser.getUsername,
+//           Password: oldPassword
+//       });
+//       cognitoUser.authenticateUser(authenticationDetails, {
+//           onSuccess: () => {
+//             cognitoUser.changePassword(oldPassword, newPassword, (err, result) => {
+//               if (err) {
+//                 alert('Fail to change password');
+//                 console.log(err);
+//               }
+//               else {
+//                   alert('Password Successfully Changed');
+//               }
+//           })}
+//       });
+//     }
+//     else {
+//       alert("No Current User");
+//     }
+//   }
+//   else {
+//       alert('Passwords do not match');
+//   }
+// }
