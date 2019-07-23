e36a407bef6dd0cff105855a21d48d7f7294c0d5
ITLetsGo <cytangao@connect.ust.hk>
2019-07-23 15:46:10 +0800

    update

--- js/job.js
+++ js/job.js
@@ -4,7 +4,7 @@ Users.authToken.then((token) => {   // check user authority
   if (token) {
     httpRequest.headers = {Authorization: token};
     let x = jwt_decode(token);
-    console.log(x);
+    console.log('yes');
   } else {
     console.log('not here');
     // window.location.href = '/signin.html';
