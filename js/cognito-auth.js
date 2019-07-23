2238400ce41b7340348299f4910a9ee8c1c4fc9d
ITLetsGo <cytangao@connect.ust.hk>
2019-07-23 17:37:25 +0800

    update

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -1,23 +1,19 @@
 
 var Users = window.Users || {};
-console.log('version 1');
 
-(function scopeWrapper($) {
-  var signinUrl = 'index.html';
-  var poolData = {
-    UserPoolId: _config.cognito.userPoolId,
-    ClientId: _config.cognito.userPoolClientId
-  };
-  var userPool;
-  if (!(_config.cognito.userPoolId &&
-        _config.cognito.userPoolClientId &&
-        _config.cognito.region)) {
-      return $('#noCognitoMessage').show();
-  }
-  userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-  if (typeof AWSCognito !== 'undefined') {
-      AWSCognito.config.region = _config.cognito.region;
-  }
+if (!(_config.cognito.userPoolId && _config.cognito.userPoolClientId && _config.cognito.region)) {
+    return alert('No Cognito Configuration');
+}
+if (typeof AWSCognito !== 'undefined') {
+  AWSCognito.config.region = _config.cognito.region;
+}
+var poolData = {
+  UserPoolId: _config.cognito.userPoolId,
+  ClientId: _config.cognito.userPoolClientId
+};
+
+(function ($) {
+  var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
   Users.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
       var cognitoUser = userPool.getCurrentUser();
@@ -39,89 +35,36 @@ console.log('version 1');
       userPool.getCurrentUser().signOut();
     };
 
-  function signin(email, password, onSuccess, onFailure) {
-      var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-          Username: email,
-          Password: password
-      });
-      var cognitoUser = createCognitoUser(email);
-      cognitoUser.authenticateUser(authenticationDetails, {
-          onSuccess: onSuccess,
-          onFailure: onFailure,
-          newPasswordRequired: function(userAttributes, requiredAttributes) {
-            cognitoUser.completeNewPasswordChallenge(password, {}, this)
-          }
-      });
-  }
-  function changePassword(oldPassword, newPassword, onSuccess, onFailure) {
-      var cognitoUser = userPool.getCurrentUser();
-      if(cognitoUser != null){
-        var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-            Username: cognitoUser.getUsername,
-            Password: oldPassword
-        });
-
-        cognitoUser.authenticateUser(authenticationDetails, {
-            onSuccess: function (){
-            cognitoUser.changePassword(oldPassword, newPassword, function(err, result) {
-              if (err) {
-                  onFailure(err);
-              } else {
-                  onSuccess(result);
-                }
-              })}
-        });
-      }
-      else {
-        alert("No Current User");
-      }
-  }
-  function createCognitoUser(email) {
-      return new AmazonCognitoIdentity.CognitoUser({
-          Username: email,
-          Pool: userPool
-      });
-  }
-
   // Event Handlers
 
   $(function onDocReady() {
       $('#signinForm').submit(handleSignin);
-      $('#changePassword').submit(handleChangePassword);
   });
 
   function handleSignin(event) {
-      var email = $('#emailInputSignin').val();
-      var password = $('#passwordInputSignin').val();
-      event.preventDefault();
-      var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-          Username: email,
-          Password: password
-      });
-      var cognitoUser = new AmazonCognitoIdentity.CognitoUser({
-          Username: email,
-          Pool: userPool
-      });
-      cognitoUser.authenticateUser(authenticationDetails, {
-          onSuccess: function(){
-            window.location.href = 'main.html';
-          },
-          onFailure: function(err){
-            alert('Login Failed');
-            console.log(err);
-          },
-          newPasswordRequired: function(userAttributes, requiredAttributes) {
-            cognitoUser.completeNewPasswordChallenge(password, {}, this)
-          }
-      });
-      // signin(email, password,
-      //      () => {
-      //       window.location.href = 'main.html';
-      //     },
-      //     err => {
-      //         alert(err);
-      //     }
-      // );
+    event.preventDefault();
+    var email = $('#emailInputSignin').val();
+    var password = $('#passwordInputSignin').val();
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username: email,
+        Password: password
+    });
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser({
+        Username: email,
+        Pool: userPool
+    });
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function(){
+          window.location.href = 'main.html';
+        },
+        onFailure: function(err){
+          alert('Login Failed');
+          console.log(err);
+        },
+        newPasswordRequired: function(userAttributes, requiredAttributes) {
+          cognitoUser.completeNewPasswordChallenge(password, {}, this)
+        }
+    });
   }
   function handleChangePassword(event) {
       var oldPassword = $('#oldPasswordInputChangePassword').val();
@@ -129,17 +72,31 @@ console.log('version 1');
       var newPassword2 = $('#newPassword2InputChangePassword').val();
       event.preventDefault();
       if (newPassword === newPassword2) {
-          changePassword(oldPassword, newPassword, function changePasswordSuccess(result){
-            console.log('call result: ' + result);
-            console.log('Password successfully changed');
-            alert('Changing Password successful. You will now be redirected to the Main page.');
-          },
-            function changePasswordError(err){
-              alert(err);
-          }
-        );
-      } else {
+        var cognitoUser = userPool.getCurrentUser();
+        if(cognitoUser != null){
+          var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+              Username: cognitoUser.getUsername,
+              Password: oldPassword
+          });
+          cognitoUser.authenticateUser(authenticationDetails, {
+              onSuccess: () => {
+                cognitoUser.changePassword(oldPassword, newPassword, (err, result) => {
+                  if (err) {
+                    alert('Fail to change password');
+                    console.log(err);
+                  }
+                  else {
+                      alert('Password Successfully Changed');
+                  }
+              })}
+          });
+        }
+        else {
+          alert("No Current User");
+        }
+      }
+      else {
           alert('Passwords do not match');
       }
-  }
+  } // prepare for future use
 }(jQuery));
