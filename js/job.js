1575922b97c636231e71b20a31496a5b40fd766c
ITLetsGo <cytangao@connect.ust.hk>
2019-07-23 15:51:37 +0800

    update

--- js/job.js
+++ js/job.js
@@ -4,7 +4,7 @@ Users.authToken.then((token) => {   // check user authority
   if (token) {
     // httpRequest.headers = {Authorization: token};
     let x = jwt_decode(token);
-    console.log(x);
+    console.log(x['cognito:username']);
   } else {
     // window.location.href = '/signin.html';
   }
