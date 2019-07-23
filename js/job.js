d02e0b86bb6146cb800971e1cdc07499a8d3a64b
ITLetsGo <cytangao@connect.ust.hk>
2019-07-23 15:40:40 +0800

    update

--- js/job.js
+++ js/job.js
@@ -1,1 +1,14 @@
 var Users = window.Users || {};
+
+Users.authToken.then((token) => {   // check user authority
+  if (token) {
+    httpRequest.headers = {Authorization: token};
+    let x = jwt_decode(token);
+    console.log(x);
+  } else {
+    // window.location.href = '/signin.html';
+  }
+}).catch((error) => {
+  // alert(error);
+  // window.location.href = '/signin.html';
+});
