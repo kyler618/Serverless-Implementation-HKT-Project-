2f7f77743abe16521ffbac5392a5c1ee45da43b0
ITLetsGo <cytangao@connect.ust.hk>
2019-07-23 18:05:05 +0800

    update

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -36,35 +36,35 @@ var poolData = {
     };
 
   $(function onDocReady() {
-      $('#signinForm').submit(signin);
+      // $('#signinForm').submit(signin);
   });
 
-  function signin(event){
-    console.log('signin');
-    event.preventDefault();
-    var email = $('#emailInputSignin').val();
-    var password = $('#passwordInputSignin').val();
-    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-        Username: email,
-        Password: password
-    });
-    var cognitoUser = new AmazonCognitoIdentity.CognitoUser({
-        Username: email,
-        Pool: userPool
-    });
-    cognitoUser.authenticateUser(authenticationDetails, {
-        onSuccess: function(){
-          window.location.href = 'main.html';
-        },
-        onFailure: function(err){
-          alert('Login Failed');
-          console.log(err);
-        },
-        newPasswordRequired: function(userAttributes, requiredAttributes) {
-          cognitoUser.completeNewPasswordChallenge(password, {}, this)
-        }
-    });
-  }
+  // function signin(event){
+  //   console.log('signin');
+  //   event.preventDefault();
+  //   var email = $('#emailInputSignin').val();
+  //   var password = $('#passwordInputSignin').val();
+  //   var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+  //       Username: email,
+  //       Password: password
+  //   });
+  //   var cognitoUser = new AmazonCognitoIdentity.CognitoUser({
+  //       Username: email,
+  //       Pool: userPool
+  //   });
+  //   cognitoUser.authenticateUser(authenticationDetails, {
+  //       onSuccess: function(){
+  //         window.location.href = 'main.html';
+  //       },
+  //       onFailure: function(err){
+  //         alert('Login Failed');
+  //         console.log(err);
+  //       },
+  //       newPasswordRequired: function(userAttributes, requiredAttributes) {
+  //         cognitoUser.completeNewPasswordChallenge(password, {}, this)
+  //       }
+  //   });
+  // }
   // Prepare for future use
   function handleChangePassword(event){
     var oldPassword = $('#oldPasswordInputChangePassword').val();
--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
 
 $(function onDocReady() {
-    console.log('indexJS', Users.userPool);
+  $('#signinForm').submit(signin);
+  function signin(event){
+      console.log('signin');
+      event.preventDefault();
+      var email = $('#emailInputSignin').val();
+      var password = $('#passwordInputSignin').val();
+      var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+          Username: email,
+          Password: password
+      });
+      var cognitoUser = new AmazonCognitoIdentity.CognitoUser({
+          Username: email,
+          Pool: userPool
+      });
+      cognitoUser.authenticateUser(authenticationDetails, {
+          onSuccess: function(){
+            window.location.href = 'main.html';
+          },
+          onFailure: function(err){
+            alert('Login Failed');
+            console.log(err);
+          },
+          newPasswordRequired: function(userAttributes, requiredAttributes) {
+            cognitoUser.completeNewPasswordChallenge(password, {}, this)
+          }
+      });
+    }
 });
