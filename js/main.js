a8930500d8c9d18a13dc156ccac4c6c150a1289a
ITLetsGo <cytangao@connect.ust.hk>
2019-07-23 10:28:34 +0800

    update

--- js/main.js
+++ js/main.js
@@ -7,8 +7,10 @@ var openOnce = false;
       let target = event.target.innerHTML;
       $('#current').html(target);
       $('#current').val(event.target.id);
-      switch(target)
-      {
+      switch(target){
+        case 'job':
+          contentBody.load( "job.html" );
+          break;
         case 'Customer': case 'Hardware':
           contentBody.load( "database.html", () => {
             if(openOnce) $.getScript("https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/js/bootstrap4-toggle.min.js"); // it is a bug
