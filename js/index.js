57c1140e6a767d3341b168418a06dfc7801a7f96
ITLetsGo <cytangao@connect.ust.hk>
2019-07-24 11:20:18 +0800

    update

--- js/index.js
+++ js/index.js
@@ -11,69 +11,70 @@ console.log('version 2');
   $(function onDocReady() {
   $('#signinForm').submit(signin);
   function signin(event){
-      event.preventDefault();
-      let poolData;
-      let email = $('#emailInputSignin').val();
-      let password = $('#passwordInputSignin').val();
-      let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-          Username: email,
-          Password: password
-      });
-      switch(email.charAt(0)){
-        case 'f':
-          poolData = {
-          UserPoolId: _config.cognito.fieldEng_userPoolId,
-          ClientId: _config.cognito.fieldEng_userPoolClientId
-        };
-          break;
-        case 's':
-          poolData = {
-          UserPoolId: _config.cognito.support_userPoolId,
-          ClientId: _config.cognito.support_userPoolClientId
-        };
-          break;
-        default:
-          alert('Invaild Email account');
-          return;
-      }
-      let userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-      window.user.b = "b";
-      window.user.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
-        var cognitoUser = userPool.getCurrentUser();
-        if (cognitoUser) {
-          cognitoUser.getSession(function sessionCallback(err, session) {
-            if (err) {
-              reject(err);
-            } else if (!session.isValid()) {
-              resolve(null);
-            } else {
-              resolve(session.getIdToken().getJwtToken());
-            }
-          });
-        } else {
-          resolve(null);
-        }
-      });
-      window.user.signOut = function signOut() {
-        userPool.getCurrentUser().signOut();
+    event.preventDefault();
+    let poolData;
+    let email = $('#emailInputSignin').val();
+    let password = $('#passwordInputSignin').val();
+    let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username: email,
+        Password: password
+    });
+    switch(email.charAt(0)){
+      case 'f':
+        poolData = {
+        UserPoolId: _config.cognito.fieldEng_userPoolId,
+        ClientId: _config.cognito.fieldEng_userPoolClientId
       };
-      let cognitoUser = new AmazonCognitoIdentity.CognitoUser({
-          Username: email,
-          Pool: userPool
-      });
-      cognitoUser.authenticateUser(authenticationDetails, {
-          onSuccess: function(){
-            window.location.href = 'main.html';
-          },
-          onFailure: function(err){
-            alert('Login Failed');
-            console.log(err);
-          },
-          newPasswordRequired: function(userAttributes, requiredAttributes) {
-            cognitoUser.completeNewPasswordChallenge(password, {}, this)
-          }
-      });
+        break;
+      case 's':
+        poolData = {
+        UserPoolId: _config.cognito.support_userPoolId,
+        ClientId: _config.cognito.support_userPoolClientId
+      };
+        break;
+      default:
+        alert('Invaild Email account');
+        return;
     }
+    let userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    window.user.b = "b";
+    console.log(window.user);
+    window.user.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
+      var cognitoUser = userPool.getCurrentUser();
+      if (cognitoUser) {
+        cognitoUser.getSession(function sessionCallback(err, session) {
+          if (err) {
+            reject(err);
+          } else if (!session.isValid()) {
+            resolve(null);
+          } else {
+            resolve(session.getIdToken().getJwtToken());
+          }
+        });
+      } else {
+        resolve(null);
+      }
+    });
+    window.user.signOut = function signOut() {
+      userPool.getCurrentUser().signOut();
+    };
+    let cognitoUser = new AmazonCognitoIdentity.CognitoUser({
+        Username: email,
+        Pool: userPool
+    });
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function(){
+          window.location.href = 'main.html';
+        },
+        onFailure: function(err){
+          alert('Login Failed');
+          console.log(err);
+        },
+        newPasswordRequired: function(userAttributes, requiredAttributes) {
+          cognitoUser.completeNewPasswordChallenge(password, {}, this)
+        }
+    });
+  }
 });
 }(jQuery));
 // Prepare for future use
