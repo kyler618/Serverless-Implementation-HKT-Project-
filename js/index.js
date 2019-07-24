59efffbf200670b16c9b5747717ae848381d0137
ITLetsGo <cytangao@connect.ust.hk>
2019-07-24 10:59:24 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var Users = window.Users || {};
-console.log('version 8');
+console.log('version 9');
 (function ($) {
   Users.a = 'a';
   if (!(_config.cognito.userPoolId && _config.cognito.userPoolClientId && _config.cognito.region)) {
