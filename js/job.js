cb6dd337dbc7310afddbf0e1d833e9612c219c69
ITLetsGo <cytangao@connect.ust.hk>
2019-07-25 11:28:04 +0800

    update

--- js/job.js
+++ js/job.js
@@ -1,14 +1,18 @@
 var Users = window.Users || {};
 Users.authToken.then((token) => {   // check user authority
   if (token) {
-    // httpRequest.headers = {Authorization: token};
-    let x = jwt_decode(token);
-    console.log(x['cognito:username']);
+    function handleResponse(results){
+      console.log(results);
+    }
+    let user = jwt_decode(token);
     var httpRequest = {
       method: 'POST',
       url: _config.api.invokeUrl +'/hkt-fieldeng-resource';,
+      headers: {Authorization: token},
       contentType: 'application/json',
+      data : {operation: "getMaintenanceRecord", target:user['cognito:username']},
       async: true ,
+      success: handleResponse,
       error: (jqXHR, textStatus, errorThrown) => {
         console.error('Error requesting: ', textStatus, ', Details: ', errorThrown);
         console.error('Response: ', jqXHR.responseText);
