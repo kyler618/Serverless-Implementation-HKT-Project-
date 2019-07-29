49c7b7721fcd20e6ed14485ba16f4fc7521fd5c8
ITLetsGo <cytangao@connect.ust.hk>
2019-07-29 13:08:29 +0800

    update

--- js/job.js
+++ js/job.js
@@ -6,7 +6,7 @@ Users.authToken.then( token => {
     function handleResponse(results){
       function createCard(id, header, content){
         return getHtml([
-          '<div class="card col-sm-3" onclick="card.card_Show(' + id.toString() + ')">',
+          '<div class="card col-sm-3" onclick="card.card_Show(\'' + id + '\')">',
             header,
             content,
           '</div>'
