15a5e59d1fff2b091b682d9fb314459976962dce
ITLetsGo <cytangao@connect.ust.hk>
2019-07-29 10:51:04 +0800

    update

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -5,38 +5,41 @@ if (typeof AWSCognito !== 'undefined') {
 }
 
 (function ($) {
-  let poolData = JSON.parse(localStorage.getItem("poolData"));
-  let userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-  Users.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
+  try{
+    let poolData = JSON.parse(localStorage.getItem("poolData"));
+    let userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    Users.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
       cognitoUser = userPool.getCurrentUser();
       if (cognitoUser) {
-          cognitoUser.getSession(function sessionCallback(err, session) {
-              if (err) {
-                  reject(err);
-              } else if (!session.isValid()) {
-                  resolve(null);
-              } else {
-                  resolve(session.getIdToken().getJwtToken());
-              }
-          });
+        cognitoUser.getSession(function sessionCallback(err, session) {
+          if (err) {
+            reject(err);
+          } else if (!session.isValid()) {
+            resolve(null);
+          } else {
+            resolve(session.getIdToken().getJwtToken());
+          }
+        });
       } else {
-          resolve(null);
+        resolve(null);
       }
-  });
-  Users.signOut = function signOut() {
-    userPool.getCurrentUser().signOut();
-    // cognitoUser.globalSignOut(  {   onFailure: e =>   console.log(e)
-    //                            , onSuccess: r =>   console.log('Logout success' + r)  })
-    localStorage.removeItem('poolData');
-    alert("You have been signed out.");
-    window.location = "index.html";
-  };
-  Users.authToken.then( token => {   // check user authority
-    if (!token) {
-      Users.signOut();
-    }
-  }).catch((error) => {
-    console.log(error);
+    });
+    Users.signOut = function signOut() {
+      userPool.getCurrentUser().signOut();
+      // cognitoUser.globalSignOut(  {   onFailure: e =>   console.log(e)
+        //                            , onSuccess: r =>   console.log('Logout success' + r)  })
+        localStorage.removeItem('poolData');
+        alert("You have been signed out.");
+        window.location = "index.html";
+      };
+    Users.authToken.then( token => {   // check user authority
+      if (!token) {
+        Users.signOut();
+      }
+    });
+
+  }
+  catch(error){
     window.location.href = 'index.html';
-  });
+  }
 }(jQuery));
