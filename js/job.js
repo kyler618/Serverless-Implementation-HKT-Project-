1b998a3b3c1c9a04a09b449bef4c202f4ae378fb
ITLetsGo <cytangao@connect.ust.hk>
2019-07-30 12:48:31 +0800

    update

--- js/job.js
+++ js/job.js
@@ -176,7 +176,7 @@ function card(){
       }
     }
     function remove_Input(event){
-      console.log(event);
+      console.log($(event));
     }
     let item = records.find(record => {
       return record.id == id
