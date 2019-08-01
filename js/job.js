2b72140dcf84d1c0a16352abdae08951e871f769
ITLetsGo <cytangao@connect.ust.hk>
2019-08-01 14:48:46 +0800

    update

--- js/job.js
+++ js/job.js
@@ -43,14 +43,15 @@ Users.authToken.then( token => {
       current_Jobs = results.Items;
       records = results.records;
       let count = 0;
-      let jobsTemp = [];
       let $jobs = current_Jobs.map( current_Job => {
         if(current_Job.end_time === undefined){
           let $cardHeader = createCardHeader(current_Job.sensor_ID);
           let $cardContent = createCardContent(current_Job.inventory_ID, current_Job.start_time);
           return createCard(current_Job.inventory_ID, $cardHeader, $cardContent);
         }
-      }).filter( current_Job => current_Job ).map( (job, index) => {
+      }).filter( current_Job => current_Job );
+      let jobsTemp = [];
+      $jobs = $jobs.map( (job, index) => {
         jobsTemp.push(job);
         if((++count%3==0 || index == ($jobs.length-1)) && jobsTemp.length != 0){
           let template = getHtml([
