3b467612d0803325cd011f792a2da32f2d5c5842
ITLetsGo <cytangao@connect.ust.hk>
2019-08-06 10:44:41 +0800

    update

--- js/database.js
+++ js/database.js
@@ -1,168 +1,6 @@
 var Users = window.Users || {};
 
-// function remove_Input(button){
-//   $(button).parent().remove();
-// }
 
-function modal(event){
-  function initialize(){
-    event.preventDefault();
-    modal.show_Modal = show_Modal;
-    var id = event.target.classList[1];
-    show_Modal(id);
-  }
-  function show_Modal(id){
-    function edit(){
-      modal.remove_Input = remove_Input;
-      $('#modal .edit').show();
-      $('#undo').click( () => {
-        modal.remove_Input = null;
-        $('#modal .edit').unbind().hide();
-        $('#edit').show().click(edit);
-        $('#complete').show().click(complete);
-        $('#modal input').attr('readonly', true);
-        $('.temporary').remove();
-        createForm();
-      });
-      $('#add').click( () => {
-        $('#form').append(getHtml([
-          '<p class="temporary">',
-          '<input type="text" class="input-group-text">',
-          '<input type="text" class="form-control">',
-          '<button class="edit" onclick="remove_Input(this)" style="display:inline"><i class="fa fa-close"></i></button>',
-          '</p>'
-        ]));
-        $('.temporary button.edit').show();
-      });
-      $('#save').click( () => {
-        function handleResponse(results){
-          if(results=="ok"){
-            // let index = records.indexOf(item);
-            // items.id = id;
-            // item = items;
-            // records[index] = items;
-            // $('#card .temporary').removeClass('temporary');
-            // $('#undo').click();
-            // httpRequest.data = JSON.stringify(data_getRecord);
-            // httpRequest.success = success_getRecord;
-            // $.ajax(httpRequest);
-          }
-        }
-        const items = {};
-        const attributes = [];
-        const input = $('#card input');
-        let changed = false;
-        for( let x = 0 ; x < input.length ; x++ ){
-          const attribute = input[x++];
-          const record = input[x];
-          if(attribute.value==""||record.value==""){
-            // items.incompleteError = x-1;
-            alert("Incompleted Error");
-            break;
-          }
-          if(attributes.includes(attribute.value)){
-            // items.DuplicateError = x-1;
-            alert("Duplicate Error");
-            break;
-          }
-          attributes.push(attribute.value);
-          items[attribute.value] = record.value;
-          if(!changed){
-            if(item[attribute.value]===undefined){
-              changed = true;
-            }
-            if(items[attribute.value]!=item[attribute.value]){
-              changed = true;
-            }
-          }
-        }
-        let deleteItem = Object.keys(item).filter( attribute => {
-          if( attribute!="id" && !(attributes.includes(attribute)) ){
-            return attribute;
-          }
-        });
-        if( !changed && deleteItem.length==0){
-          return $('#undo').click();
-        }
-        const data = {table:"Hardware", operation: "maintainSensor", pk: id};
-        data.input = (changed)? items:null;
-        data.delete = (deleteItem.length!=0)? deleteItem:null;
-        httpRequest.data = JSON.stringify(data);
-        httpRequest.success = handleResponse;
-        $.ajax(httpRequest);
-      });
-      $('#modal .form-control').removeAttr('readonly');
-      $('#modal .input-group-text:not(.readonly)').removeAttr('readonly');
-      $('#edit').unbind().hide();
-      $('#complete').unbind().hide();
-    }
-    function createForm(){
-      function createInput(attribute, record){
-        return getHtml([
-          '<p class="addition">',
-          '<input type="text" class="input-group-text" value=\'' + attribute + '\' readonly>',
-          '<input type="text" class="form-control" name=\'' + attribute + '\' value=\'' + record + '\' readonly>',
-          '<button class="edit" onclick="modal.remove_Input(this)"><i class="fa fa-close"></i></button>',
-          '</p>'
-        ]);
-      }
-      for(let key in item){
-        if(key=="id") continue;
-        let $input = $('#modal input.form-control[name=\'' + key + '\']');
-        if($input.length != 0 ){
-          $('#modal input.input-group-text[value=\'' + key + '\']').val(key); // for undo
-          $input.val(item[key]);
-        }
-        else{
-          $('#form').append(createInput(key, item[key]));
-        }
-      }
-    }
-    function complete(){
-      function handleResponse(results){
-        if(results=="ok"){
-          httpRequest.data = JSON.stringify(data_getRecord);
-          httpRequest.success = function(results){
-            success_getRecord(results);
-            $('#quit').click();
-          }
-          $.ajax(httpRequest);
-        }
-      }
-      // let target = current_Jobs.find(current_Job => {
-      //   return current_Job.inventory_ID == id;
-      // });
-      data_confirm = {operation: "completeMaintenance", table: "Maintenance", target: id};
-      httpRequest.data = JSON.stringify(data_confirm);
-      httpRequest.success = handleResponse;
-      $.ajax(httpRequest);
-    };
-    function remove_Input(button){
-      $(button).parent().remove();
-    }
-    let item = storedItem.find(record => {
-    return record.id == id
-  });
-    $('#modal').show();
-    $('#edit').click(edit);
-    $('#quit').click( () => {
-      $('#edit').unbind().show();
-      $('#complete').unbind().show();
-      $('#modal .edit').unbind().hide();
-      $('#modal').hide();
-      $('.addition').remove();
-      $('.temporary').remove();
-      $('#modal .form-control').val('');
-      modal.show_Modal = null;
-      modal.remove_Input = null;
-      $('#quit').unbind();
-    } );
-    createForm();
-  };
-  initialize();
-}
-
-(function ($) {
   user_Identity();
   const selector = [];
   const attributes = [];
@@ -1005,8 +843,161 @@ function modal(event){
   //
   // }
 
-  function remove_Input(button){
-    $(button).parent().remove();
+  function modal(event){
+    function initialize(){
+      event.preventDefault();
+      modal.show_Modal = show_Modal;
+      var id = event.target.classList[1];
+      show_Modal(id);
+    }
+    function show_Modal(id){
+      function edit(){
+        $('#modal .edit').show();
+        $('#undo').click( () => {
+          $('#modal .edit').unbind().hide();
+          $('#edit').show().click(edit);
+          $('#complete').show().click(complete);
+          $('#modal input').attr('readonly', true);
+          $('.temporary').remove();
+          createForm();
+          modal.remove_Input = null;
+        });
+        $('#add').click( () => {
+          $('#form').append(getHtml([
+            '<p class="temporary">',
+            '<input type="text" class="input-group-text">',
+            '<input type="text" class="form-control">',
+            '<button class="edit" onclick="modal.remove_Input(this)" style="display:inline"><i class="fa fa-close"></i></button>',
+            '</p>'
+          ]));
+          $('.temporary button.edit').show();
+        });
+        $('#save').click( () => {
+          function handleResponse(results){
+            if(results=="ok"){
+              // let index = records.indexOf(item);
+              // items.id = id;
+              // item = items;
+              // records[index] = items;
+              // $('#card .temporary').removeClass('temporary');
+              // $('#undo').click();
+              // httpRequest.data = JSON.stringify(data_getRecord);
+              // httpRequest.success = success_getRecord;
+              // $.ajax(httpRequest);
+            }
+          }
+          const items = {};
+          const attributes = [];
+          const input = $('#card input');
+          let changed = false;
+          for( let x = 0 ; x < input.length ; x++ ){
+            const attribute = input[x++];
+            const record = input[x];
+            if(attribute.value==""||record.value==""){
+              // items.incompleteError = x-1;
+              alert("Incompleted Error");
+              break;
+            }
+            if(attributes.includes(attribute.value)){
+              // items.DuplicateError = x-1;
+              alert("Duplicate Error");
+              break;
+            }
+            attributes.push(attribute.value);
+            items[attribute.value] = record.value;
+            if(!changed){
+              if(item[attribute.value]===undefined){
+                changed = true;
+              }
+              if(items[attribute.value]!=item[attribute.value]){
+                changed = true;
+              }
+            }
+          }
+          let deleteItem = Object.keys(item).filter( attribute => {
+            if( attribute!="id" && !(attributes.includes(attribute)) ){
+              return attribute;
+            }
+          });
+          if( !changed && deleteItem.length==0){
+            return $('#undo').click();
+          }
+          const data = {table:"Hardware", operation: "maintainSensor", pk: id};
+          data.input = (changed)? items:null;
+          data.delete = (deleteItem.length!=0)? deleteItem:null;
+          httpRequest.data = JSON.stringify(data);
+          httpRequest.success = handleResponse;
+          $.ajax(httpRequest);
+        });
+        $('#modal .form-control').removeAttr('readonly');
+        $('#modal .input-group-text:not(.readonly)').removeAttr('readonly');
+        $('#edit').unbind().hide();
+        $('#complete').unbind().hide();
+      }
+      function createForm(){
+        function createInput(attribute, record){
+          return getHtml([
+            '<p class="addition">',
+            '<input type="text" class="input-group-text" value=\'' + attribute + '\' readonly>',
+            '<input type="text" class="form-control" name=\'' + attribute + '\' value=\'' + record + '\' readonly>',
+            '<button class="edit" onclick="modal.remove_Input(this)"><i class="fa fa-close"></i></button>',
+            '</p>'
+          ]);
+        }
+        for(let key in item){
+          if(key=="id") continue;
+          let $input = $('#modal input.form-control[name=\'' + key + '\']');
+          if($input.length != 0 ){
+            $('#modal input.input-group-text[value=\'' + key + '\']').val(key); // for undo
+            $input.val(item[key]);
+          }
+          else{
+            $('#form').append(createInput(key, item[key]));
+          }
+        }
+      }
+      function complete(){
+        function handleResponse(results){
+          if(results=="ok"){
+            httpRequest.data = JSON.stringify(data_getRecord);
+            httpRequest.success = function(results){
+              success_getRecord(results);
+              $('#quit').click();
+            }
+            $.ajax(httpRequest);
+          }
+        }
+        // let target = current_Jobs.find(current_Job => {
+        //   return current_Job.inventory_ID == id;
+        // });
+        data_confirm = {operation: "completeMaintenance", table: "Maintenance", target: id};
+        httpRequest.data = JSON.stringify(data_confirm);
+        httpRequest.success = handleResponse;
+        $.ajax(httpRequest);
+      };
+      function remove_Input(button){
+        $(button).parent().remove();
+      }
+      let item = storedItem.find(record => {
+      return record.id == id
+    });
+      $('#modal').show();
+      $('#edit').click(edit);
+      $('#quit').click( () => {
+        $('#edit').unbind().show();
+        $('#complete').unbind().show();
+        $('#modal .edit').unbind().hide();
+        $('#modal').hide();
+        $('.addition').remove();
+        $('.temporary').remove();
+        $('#modal .form-control').val('');
+        modal.remove_Input = null;
+        modal.show_Modal = show_Modal;
+        $('#quit').unbind();
+      } );
+      createForm();
+    };
+    initialize();
   }
 
   function handleTestClick(event) {
@@ -1093,4 +1084,3 @@ function modal(event){
   function getHtml(template) {
     return template.join('\n');
   }
-}(jQuery));
