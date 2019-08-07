c721ff092a22ab9af4208af07984a6f56891efc4
ITLetsGo <cytangao@connect.ust.hk>
2019-08-07 15:39:37 +0800

    update

--- js/database.js
+++ js/database.js
@@ -844,7 +844,7 @@ var Users = window.Users || {};
           }
         });
         headers = headers.filter( header => header );
-        return headers;
+        return getHtml([headers]);
         attr.forEach( field => {
           if( !attributes.includes(field) ){
             attributes.push(field);
