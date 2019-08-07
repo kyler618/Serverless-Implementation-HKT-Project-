ce5c99bb153de56eaddc54977047286d8564582e
ITLetsGo <cytangao@connect.ust.hk>
2019-08-07 15:43:23 +0800

    update

--- js/database.js
+++ js/database.js
@@ -836,6 +836,7 @@ var Users = window.Users || {};
     function insertHeaderRow(){
       // const headerRow = document.querySelector("table thead tr");
       const attrs = storedItem.map(item => Object.keys(item));
+      console.log(attrs);
       let _headers = attrs.map( attr => {
         let headers = attr.map( field => {
           if( !attributes.includes(field) ){
