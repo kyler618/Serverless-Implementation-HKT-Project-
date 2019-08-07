a02830f88a70b84064369627c3f490829b8a85d6
ITLetsGo <cytangao@connect.ust.hk>
2019-08-07 18:06:03 +0800

    update

--- js/database.js
+++ js/database.js
@@ -256,7 +256,7 @@ var Users = window.Users || {};
       $('.editMode').show().unbind();
 
       $("#table tbody input").removeAttr("readOnly").change( event => {
-        let $target = $(event.target).parent();
+        let $target = $(event.target).parent().parent();
         console.log($target);
         if(!changedRecords.includes(event.target.classList[1]))
         {
