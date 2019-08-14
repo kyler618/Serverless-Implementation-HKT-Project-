5c71a93331cae85613a7f43d1d17eadeb5f740d6
ITLetsGo <cytangao@connect.ust.hk>
2019-08-14 23:39:36 +0800

    update

--- js/job.js
+++ js/job.js
@@ -180,7 +180,7 @@ function card(){
         data.input = (changed)? items:null;
         data.delete = (deleteItem.length!=0)? deleteItem:null;
         httpRequest.data = null;
-        // httpRequest.data = JSON.stringify(data);
+        httpRequest.data = JSON.stringify(data);
         // httpRequest.data = JSON.stringify(data);
         httpRequest.success = handleResponse;
         console.log(httpRequest);
