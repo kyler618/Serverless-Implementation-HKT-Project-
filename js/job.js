ec467226a9f30b7d465a21a247b9084ef4afd069
ITLetsGo <cytangao@connect.ust.hk>
2019-08-15 00:51:01 +0800

    update

--- js/job.js
+++ js/job.js
@@ -127,9 +127,9 @@ function card(){
             records[index] = items;
             $('#card .temporary').removeClass('temporary');
             $('#undo').click();
-            // httpRequest.data = JSON.stringify(data_getRecord);
-            // httpRequest.success = success_getRecord;
-            // $.ajax(httpRequest);
+            httpRequest.data = JSON.stringify(data_getRecord);
+            httpRequest.success = success_getRecord;
+            $.ajax(httpRequest);
           }
         }
         const items = {};
