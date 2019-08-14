e11a159f775b2801aaaded6d9ea860fc7523810e
ITLetsGo <cytangao@connect.ust.hk>
2019-08-14 15:15:48 +0800

    update

--- js/database.js
+++ js/database.js
@@ -218,7 +218,7 @@ var Users = window.Users || {};
       }
       searchItem();
     }
-    $("#table").bind("keypress", event => {
+    $("#modal").bind("keypress", event => {
       if(event.keyCode === 13)
       {
         event.preventDefault();
